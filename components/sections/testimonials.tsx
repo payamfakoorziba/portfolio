c12c5ee4f1c6fc46cdb5ec7b684990ca895c065f
Payam Fakoorziba <payamfakoor@gmail.com>
2024-03-13 16:07:31 -0400

    eslint cheese

--- components/sections/testimonials.tsx
+++ components/sections/testimonials.tsx
@@ -26,11 +26,8 @@ const testimonials = [
 
 const Testimonials = () => {
   return (
-    <Container>
-      <h3
-        className="font-medium text-5xl md:text-6xl scroll-mt-10"
-        id="testimonials"
-      >
+    <Container id="testimonials">
+      <h3 className="font-medium text-5xl md:text-6xl scroll-mt-10">
         Testimonials
       </h3>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-8">
