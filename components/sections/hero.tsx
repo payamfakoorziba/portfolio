c6e4a6d9f14a86c10086c7cc5e92f331906e6516
Payam Fakoorziba <payamfakoor@gmail.com>
2024-03-16 23:24:17 -0400

    animate hero

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Container from "../ui/container";
 import ProfilePic from "@/public/profile-pic.jpeg";
-import { Button } from "../ui/button";
+import { motion } from "framer-motion";
 import GetInTouch from "../get-in-touch-button";
 
 const Hero = () => {
@@ -9,22 +11,38 @@ const Hero = () => {
     <Container>
       <div className="mt-20 md:mt-0 flex flex-col-reverse gap-y-10 md:flex-row justify-between items-center pt-20 md:pt-40 gap-x-10 mb-10">
         <div className="flex flex-col flex-1 items-start md:items-start md:text-start">
-          <h1 className="text-5xl md:text-6xl font-semibold tracking-tight">
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.1, duration: 0.3 }}
+            className="text-5xl md:text-6xl font-semibold tracking-tight"
+          >
             Hi, I&apos;m Payam
-          </h1>
-          <h2 className="text-lg md:text-2xl font-light tracking-tighter mt-2 md:mt-6 text-white/70">
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2, duration: 0.3 }}
+            className="text-lg md:text-2xl font-light tracking-tighter mt-2 md:mt-4 text-white/70"
+          >
             Web designer and developer
-          </h2>
-          <p className="text-sm md:text-base mt-6 md:mt-12 leading-8 ">
-            I design and build custom websites tailored to elevate your
-            business. <br className="hidden lg:block" />
-            Ready for a website that stands out?{" "}
-            <br className="block sm:hidden" />
-            Let&apos;s talk.
-          </p>
-          <GetInTouch className="mt-8 md:mt-12" />
+          </motion.h2>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.8, ease: "linear" }}
+          >
+            <p className="text-sm md:text-base mt-6 md:mt-12 leading-8">
+              I design and build custom websites tailored to elevate your
+              business. <br className="hidden lg:block" />
+              Ready for a website that stands out?{" "}
+              <br className="block sm:hidden" />
+              Let&apos;s talk.
+            </p>
+            <GetInTouch className="mt-6 md:mt-12" />
+          </motion.div>
         </div>
-        <div className="hidden md:block rounded-full relative size-24 md:size-[300px] lg:size-[350px] overflow-hidden shadow-md shadow-white/5 flex-shrink-0">
+        <motion.div className="hidden md:block rounded-full relative size-24 md:size-[300px] lg:size-[350px] overflow-hidden shadow-md shadow-white/5 flex-shrink-0">
           <div className="inset-0 bg-black/15 absolute z-10" />
           <Image
             src={ProfilePic}
@@ -33,7 +51,7 @@ const Hero = () => {
             fill
             className="object-cover -scale-x-100"
           />
-        </div>
+        </motion.div>
       </div>
     </Container>
   );
